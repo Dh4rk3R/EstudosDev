f3a6975c8e09f0a864c5e3068f68ab37e8ef4a70
Kaio Maia <kaio.venicios2014@gmail.com>
2024-09-06 01:25:12 -0300

    Criação de cenários HTML

---
+++ app.js
@@ -0,0 +1,3 @@
+
+
+console.log(dados[0].descricao)
---
+++ dados.js
@@ -0,0 +1,52 @@
+
+
+let dados = [
+    {
+        titulo : "Homem de Ferro",
+        descricao : "O Homem de Ferro é um super-herói da Marvel Comics e um dos pilares do Universo Cinematográfico Marvel. Por trás da armadura, está Tony Stark, um bilionário, engenheiro e inventor. Após ser sequestrado e forçado a construir uma arma devastadora, Stark cria uma armadura de alta tecnologia para escapar e decide usar seus poderes para o bem. A combinação de inteligência, tecnologia e carisma faz de Tony Stark um dos personagens mais populares da Marvel, conhecido por sua jornada de redenção, seu senso de humor irônico e sua dedicação à proteção da humanidade.",
+        link : "https://pt.wikipedia.org/wiki/Homem_de_Ferro"
+    
+    },
+    {
+        titulo : "Homem-Aranha",
+        descricao : "Picado por uma aranha radioativa, Peter Parker se tornou o Homem-Aranha, um super-herói com habilidades sobre-humanas como força, agilidade e a capacidade de aderir a superfícies. Sob a máscara, é um jovem estudante que enfrenta os desafios da adolescência enquanto luta contra o crime em Nova York. Com um senso de humor sarcástico e um coração puro, o Homem-Aranha é um dos personagens mais queridos da Marvel, conhecido por sua luta constante entre seus deveres como herói e sua vida pessoal",
+        link : "https://pt.wikipedia.org/wiki/Homem-Aranha"
+    
+    },
+    {
+        titulo : "Thor",
+        descricao : "Príncipe de Asgard e filho de Odin, Thor é um poderoso deus nórdico que empunha o martelo Mjolnir. Banido para a Terra, Thor aprendeu a humildade e a responsabilidade, tornando-se um defensor da humanidade. Com força sobre-humana, resistência e a capacidade de controlar o clima, Thor é um dos Vingadores mais poderosos, conhecido por sua nobreza, senso de justiça e sua luta constante contra as forças das trevas.",
+        link : "https://pt.wikipedia.org/wiki/Thor"
+    
+    },
+    {
+        titulo: "Capitã Marvel",
+        descricao: "Uma das heroínas mais poderosas do Universo Marvel, Carol Danvers ganhou seus poderes cósmicos após um acidente. Com força sobre-humana, a capacidade de voar e manipular energia, a Capitã Marvel é uma líder inspiradora e uma força a ser reconhecida. Sua história explora temas de identidade, empoderamento feminino e a busca por seu lugar no universo.",
+        link: "https://pt.wikipedia.org/wiki/Capitã_Marvel"
+        },
+
+    {
+        titulo: "Pantera Negra",
+        descricao: "Rei de Wakanda, T'Challa é o protetor de sua nação tecnologicamente avançada. Com habilidades sobre-humanas obtidas através da Erva em Forma de Coração, o Pantera Negra é um guerreiro habilidoso e um líder nato. Sua história explora temas de cultura, identidade e a luta por justiça social.",
+        link: "https://pt.wikipedia.org/wiki/Pantera_Negra"
+    },
+
+    {
+        titulo: "Hulk",
+        descricao: "Exposto a raios gama durante um experimento, Bruce Banner se transforma no Hulk, uma criatura de força inimaginável. A raiva desencadeia sua transformação, e o Hulk é frequentemente retratado como uma força da natureza, tanto destrutiva quanto protetora. Sua história explora temas de raiva, controle e a dualidade da natureza humana.",
+        link: "https://pt.wikipedia.org/wiki/Hulk"
+    },
+
+    {
+        titulo: "Visão",
+        descricao: "Criado por Ultron, a Visão é um androide com poderes mentais e a capacidade de manipular a densidade de seu corpo. Como membro dos Vingadores, a Visão busca entender sua própria existência e seu lugar no mundo. Sua história explora temas de inteligência artificial, filosofia e a natureza da consciência.",
+        link: "https://pt.wikipedia.org/wiki/Visão"
+    },
+
+    {
+        titulo: "Feiticeira Escarlate",
+        descricao: "Uma mutante com a capacidade de manipular a realidade, a Feiticeira Escarlate é uma das heroínas mais poderosas e complexas da Marvel. Sua história é marcada por traumas e perdas, e seus poderes a colocam em uma posição de grande responsabilidade. Sua história explora temas de poder, perda e a natureza da realidade.",
+        link: "https://pt.wikipedia.org/wiki/Feiticeira_Escarlate"
+    }
+];
+
