3b3a4a53979645ca5f1af5f46a4bf3df9dcb8f74
Kaio Maia <kaio.venicios2014@gmail.com>
2024-09-07 10:52:11 -0300

    Novas modificações realizadas dentro do sistemas de estudo

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    // Inicializa uma string para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        // Cria um elemento div para cada resultado
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank">Saiba mais!</a>
+                </h2>
+            </div>
+        `;
+    }
+
+    // Atualiza o conteúdo da seção com os resultados da pesquisa
+    section.innerHTML = resultados;
+}
 
 
-console.log(dados[0].descricao)
+// <img src="${dado.imagem}"> - Pedaço do cód que não teve funcionamento..
