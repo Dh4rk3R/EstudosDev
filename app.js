7dce84d8fb956480ca27d41304107ea8e6b890bb
Kaio Maia <kaio.venicios2014@gmail.com>
2024-09-08 00:36:48 -0300

    Atualizações Projeto

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 function pesquisar() {
     // Obtém a seção onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
+   
     // Inicializa uma string para armazenar os resultados da pesquisa
-    let resultados = "";
+    let campoPesquisa = document.getElementById("campoPesquisa").value
+    
 
+
+    //Condição par validar se vazio
+    if(!campoPesquisa == ''){
+        section.innerHTML = "<h2> Nada foi encontrado T-T</h2>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    console.log(campoPesquisa);
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+    
     // Itera sobre cada dado na lista de dados
     for (let dado of dados) {
+        titulo = dado.titulo.toLocaleLowerCase();
+        descricao = dado.descricao.toLocaleLowerCase();
+        tags = dado.tags.toLocaleLowerCase();
+        //se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+                    //então faça isso..
+        console.log(dado.titulo.includes(campoPesquisa))
         // Cria um elemento div para cada resultado
         resultados += `
             <div class="item-resultado">
@@ -14,12 +39,16 @@ function pesquisar() {
                     <p class="descricao-meta">${dado.descricao}</p>
                     <a href="${dado.link}" target="_blank">Saiba mais!</a>
                 </h2>
-            </div>
-        `;
-    }
+            </div>`;
+        }
+         if(!resultados){
+        resultados = "<h2>Dados não encontrado!</h2>"
+        }
 
-    // Atualiza o conteúdo da seção com os resultados da pesquisa
-    section.innerHTML = resultados;
+        // Atualiza o conteúdo da seção com os resultados da pesquisa
+        section.innerHTML = resultados;
+    
+    }
 }
 
 
